ce6c8838bb713fd33ff0e50cd94f1f1ad20a687e
Cynthia Wasonga <wasonga.cynthia@yahoo.in>
2017-01-17 17:42:28 +0300

    Added function to test Polymorphism (Overriding of Parent attributes

--- elephant.py
+++ elephant.py
@@ -4,12 +4,11 @@ class Elephant(Animal):
 
 	def __init__(self, given_name, type, age, location, endangered):
 		# We already know some qualities of an elephant
-		# Calling the parent constructor Animal to set the main attributes
-		super().__init__("Elephant", "Mammal", 4, "herbivore", endangered)
 		self.given_name = given_name
 		self.type = type
 		self.age = age
 		self.location = location
+		self.endangered = endangered
 
 	def setGivenName(self, given_name):
 		self.given_name = given_name
@@ -35,7 +34,18 @@ class Elephant(Animal):
 	def getLocation(self):
 		return self.location
 
+
 	def describeElephant(self):
+		# Calling the parent constructor Animal to set the main attributes
+		super().__init__("Elephant", "Mammal", 4, "herbivore", self.endangered)
+		return("This %s is a %s.\nFeeding Habits: %s\nNo of Legs: %d\nEndangered: %s \
+			\nGiven Name: %s\nType of Elephant: %s\nAge: %d\nLocation: %s"
+			%(self.getName(), self.getAnimalClass(), self.getFeedingHabit(),
+			 self.getNoOfLegs(), self.isEndangered(), self.given_name, self.type,
+			 self.age, self.location))
+
+	def showPolymorphism(self):
+		print("\n ---- Testing Polymorphism... ----")
 		return("This %s is a %s.\nFeeding Habits: %s\nNo of Legs: %d\nEndangered: %s \
 			\nGiven Name: %s\nType of Elephant: %s\nAge: %d\nLocation: %s"
 			%(self.getName(), self.getAnimalClass(), self.getFeedingHabit(),
@@ -49,8 +59,18 @@ class Elephant(Animal):
 
 
 if __name__ == '__main__':
-	elephant = Elephant("Tatu", "African", 50, "Maasai Mara", True)
-	print(elephant.describeElephant())
+	try:
+		elephant = Elephant("Tatu", "African", 50, "Maasai Mara", True)
+		print(elephant.describeElephant())
+	except Exception as e:
+		print("Error: ", e)
+
+	try:
+		elephant = Elephant("Tatu", "African", 50, "Maasai Mara", True)
+		print(elephant.showPolymorphism())
+	except Exception as e:
+		print("Error! Base Class Attributes overriden: ", e)
+
 	try:
 		print(elephant.testEncapsulation())
 	except AttributeError as e:
