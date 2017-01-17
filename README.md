b7c0547dfa4d9fa68f5e5d91fa217e1d315a6e43
Cynthia Wasonga <wasonga.cynthia@yahoo.in>
2017-01-17 17:34:01 +0300

    Updated README with details on Inheritance and Encapsulation

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # Endangered Species - Python OPP Demonstration
 
 Solving the problem of tracking endangered animals in an animal park using OOP. Demonstrates inheritance, encapsulation, polymorphism and various OOP concepts.
+
+## The Base Class
+
+The Base Class or the Parent Class is the `Animal` Class. This class holds generic information concerning animals. Other classes inherit its attributes and metthods.
+
+## The Subclass
+
+The Subclass or child class is the `Elephant` Class. This class inherits the basic attributes of an Animal from the `Animal` parent class.
+
+## Inheritance
+This is when a Class takes up the non-private attributes and methods of another class. It prevents a lot of repetition, especially when different object with some similar properties need to be used. In this case, the Elephant Class takes up the properties of the `Animal` base class.
+
+## Encapsulation
+This is done in order to prevent sensitive data from being accessed by outer classes. In python, this is done by prefixing the attribute or method with a `__`. 
+
+For example, in  the `Animal` class, the `name` and `animal_class` attributes are private attributes and can only be accessed by the `Animal` class, but not by its subclass `Elephant`. 
+The method `testEncapsulation()` has been used to demonstrate this.
+ 
--- elephant.py
+++ elephant.py
@@ -2,13 +2,12 @@ from animal_class import Animal
 
 class Elephant(Animal):
 
-	def __init__(self, given_name, type, age, location, endangered, has_tusks=True):
+	def __init__(self, given_name, type, age, location, endangered):
 		# We already know some qualities of an elephant
 		# Calling the parent constructor Animal to set the main attributes
 		super().__init__("Elephant", "Mammal", 4, "herbivore", endangered)
 		self.given_name = given_name
 		self.type = type
-		self.has_tusks = has_tusks
 		self.age = age
 		self.location = location
 
@@ -38,13 +37,13 @@ class Elephant(Animal):
 
 	def describeElephant(self):
 		return("This %s is a %s.\nFeeding Habits: %s\nNo of Legs: %d\nEndangered: %s \
-			\nGiven Name: %s\nType of Elephant: %s\nHas Tusks: %s\nAge: %d\nLocation: %s"
+			\nGiven Name: %s\nType of Elephant: %s\nAge: %d\nLocation: %s"
 			%(self.getName(), self.getAnimalClass(), self.getFeedingHabit(),
-			 self.getNoOfLegs(), self.isEndangered(), self.given_name, self.type, self.has_tusks,
+			 self.getNoOfLegs(), self.isEndangered(), self.given_name, self.type,
 			 self.age, self.location))
 
 	def testEncapsulation(self):
-		print("Testing Encapsulation...")
+		print("\n ---- Testing Encapsulation... ----")
 		return("The name of this animal is: %s" % self.__name)
 
 
@@ -52,4 +51,7 @@ class Elephant(Animal):
 if __name__ == '__main__':
 	elephant = Elephant("Tatu", "African", 50, "Maasai Mara", True)
 	print(elephant.describeElephant())
-	print(elephant.testEncapsulation())
+	try:
+		print(elephant.testEncapsulation())
+	except AttributeError as e:
+		print("Encapsulation test: ", e)
